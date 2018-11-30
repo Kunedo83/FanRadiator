50c3c4f750d75fc49759634fd9e59be44601e350
Kunedo83 <45496737+Kunedo83@users.noreply.github.com>
2018-11-30 22:19:21 +0100

    Add files via upload

---
+++ SourceCode/SourceCode/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Kunedo
+{
+    public class Utils
+    {
+        public static AnimationState[] SetUpAnimation(string animationName, Part part)  //Thanks Majiir!
+        {
+            var states = new List<AnimationState>();
+            foreach (var animation in part.FindModelAnimators(animationName))
+            {
+                var animationState = animation[animationName];
+                animationState.speed = 0;
+                animationState.enabled = true;
+                animationState.wrapMode = WrapMode.ClampForever;
+                animation.Blend(animationName);
+                states.Add(animationState);
+            }
+            return states.ToArray();
+        }        
+    }
+}
