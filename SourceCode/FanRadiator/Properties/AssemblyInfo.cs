d9a2ed5f9192f591ad48948e58e778281bfa2303
Kunedo83 <45496737+Kunedo83@users.noreply.github.com>
2018-12-21 15:58:30 +0100

    Update AssemblyInfo.cs

--- SourceCode/FanRadiator/Properties/AssemblyInfo.cs
+++ SourceCode/FanRadiator/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los números de compilación y de revisión predeterminados
 // mediante el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.5.1")]
-[assembly: AssemblyFileVersion("0.0.0.7")]
+[assembly: AssemblyVersion("0.1.6.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
 [assembly: NeutralResourcesLanguage("es-ES")]
 
